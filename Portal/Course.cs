fa7c266d5ecf0e78c754da09d365907bc65883b0
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-07-13 15:51:06 +0300

    add class files, according to the entity schema

---
+++ Portal/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Material> Materials { get; set; }
+        public List<Skill> CourseSkillList { get; set; }
+    }
+}
