8a570cb775789ccfcdf0688ea1c33775b577521e
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-08-05 13:57:51 +0300

    little fixes of authorization

--- BasicFunctionality/User.cs
+++ BasicFunctionality/User.cs
@@ -6,20 +6,12 @@ namespace BasicFunctionality
 {
     public class User
     {
-<<<<<<< HEAD
         public User(string email, string password, string name)
-=======
-        public User(string email, string password, string name, int id)
->>>>>>> f136b713f83d82b3d848ff98d1c6a9f2dd23abca
         {
             this.Email = email;
             this.Password = password;
             this.Name = name;
-<<<<<<< HEAD
             this.Id = SetId();
-=======
-            this.Id = id;
->>>>>>> f136b713f83d82b3d848ff98d1c6a9f2dd23abca
         }
 
         public string Email { get; internal set; }
@@ -29,7 +21,6 @@ namespace BasicFunctionality
         public string Name { get; internal set; }
 
         public int Id { get; set; }
-<<<<<<< HEAD
 
         private static int SetId()
         {
@@ -49,7 +40,5 @@ namespace BasicFunctionality
 
             return id;
         }
-=======
->>>>>>> f136b713f83d82b3d848ff98d1c6a9f2dd23abca
     }
 }
