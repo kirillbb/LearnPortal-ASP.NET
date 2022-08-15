97343a066f3398de32cdd5ac0a521cbd042710d5
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-08-15 16:13:39 +0300

    add course operation

--- basicFunctions_DB/LogicLayer/Operations/CourseOperation.cs
+++ basicFunctions_DB/LogicLayer/Operations/CourseOperation.cs
@@ -1,4 +1,8 @@
-﻿using basicFunctions_DB.FacadeLayer;
+﻿using basicFunctions_DB.DataLayer;
+using basicFunctions_DB.DataLayer.CourseType;
+using basicFunctions_DB.FacadeLayer;
+using basicFunctions_DB.GenericRepository;
+using basicFunctions_DB.LogicLayer.Authorization;
 
 namespace basicFunctions_DB.LogicLayer.Operations
 {
@@ -53,27 +57,102 @@ namespace basicFunctions_DB.LogicLayer.Operations
 
         private static void CreateCourse()
         {
-            throw new NotImplementedException();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                GenericRepository<Course> repository = new GenericRepository<Course>(db);
+
+                Course course;
+                Console.WriteLine("Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Description:");
+                string description = Console.ReadLine();
+
+                course = new Course
+                {
+                    Creator = db.Users.Where(x => x.Id == AuthorizationControler.AuthorizatedUser.Id).FirstOrDefault(),
+                    Name = name,
+                    Description = description
+                };
+
+                repository.Insert(course);
+                repository.Save();
+            }
         }
 
         private static void FindById()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter ID:");
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                GenericRepository<Course> repository = new GenericRepository<Course>(db);
+
+                string id = Console.ReadLine();
+                var course = repository.GetById(int.Parse(id));
+
+                Console.WriteLine("ID: " + course.Id + " \"" + course.Name + "\" " + course.Description);
+
+                Console.ReadLine();
+            }
         }
 
         private static void UpdateCourse()
         {
-            throw new NotImplementedException();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                GenericRepository<Course> repository = new GenericRepository<Course>(db);
+
+                Console.WriteLine("Enter ID:");
+                string id = Console.ReadLine();
+                var course = repository.GetById(int.Parse(id));
+
+                Console.WriteLine($"Current Name = {course.Name} enter a new Name:");
+                course.Name = Console.ReadLine();
+                Console.WriteLine($"Current Description = {course.Description} enter a new Description:");
+                course.Description = Console.ReadLine();
+
+                repository.Update(course);
+
+                repository.Save();
+            }
+
+            Console.ReadLine();
+            Control();
         }
 
         private static void PrintAll()
         {
-            throw new NotImplementedException();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                GenericRepository<Course> repository = new GenericRepository<Course>(db);
+
+                Console.WriteLine("List of all materials:");
+                var list = repository.GetAll();
+                foreach (var course in list)
+                {
+                    Console.WriteLine("ID: " + course.Id + " " + course.Name + " - " + course.Description);
+                }
+
+                Console.ReadLine();
+                Control();
+            }
         }
 
         private static void DeleteCourse()
         {
-            throw new NotImplementedException();
+            int id = 0;
+            Console.WriteLine("Enter course-ID to remove:");
+            int.TryParse(Console.ReadLine(), out id);
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                GenericRepository<Course> repository = new GenericRepository<Course>(db);
+                repository.Delete(id);
+                repository.Save();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Control();
         }
     }
 }
